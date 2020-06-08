74e2d5e6f147a3bd0254ef40067f572da83f06ab
namespaceJav <53064453+namespaceJav@users.noreply.github.com>
2020-06-07 19:36:53 -0500

    Add files via upload
    When writing desktop apps, gui or not, there is a handy console that you can use to print debug information while your app is running.
    
    Android however is all about a graphical interface, and doesnt provide apps with a console.
    
    This app solves that problem.
    1. Install the app.
    2. Place the IConsole.java header in the correct path as per its package name.
    3. IConsole has all the methods needed to link your app to Android Console and print data to Android Console.
           A: Call IConsole(Context);  eg IConsole con = new IConsole(activity);
           B: use writeString(String), writeInt(int), etc. eg: con.writeString("Hello Android Console");
           C: call disconnect() when it is no longer needed. 
                 eg: public void onDestroy() { con.disconnect(); }
    
    Notes:
     - IConsole implements finalize, so I assume it is not necessary to manually call disconnect.
     - Closing the Android Console will automatically clean up any client connected.
     - disconnecting from IConsole doesnt destroy the Android Console so you still get to view the print outs
    
    Pending Improvements
     - Android Console will fail to pop up with print outs until, while the client is writing to it. In future, this blocking behaviour should be fixed.
     - Ability to set Uncaught Exception handler right from IConsole. In which uncaught exceptions are printed to Android Console.
     - Menu button on Android Console hides it and doesnt actually present a menu.
     - A button to hide the console is useful, as Android Console consumes keyboard focus if it is on top of your app.
     - Android Console will always get hidden, as soon as you switch to another app, including the app in question.  Future versions should allow Android Console to remain on top.
    
    All in all. This is a super useful app and it has aided me alot in developing my apps. Even if you know how to use a debugger, I am certain a debugger wont help you to understand logic errors of your app.

---
+++ IConsole.java
@@ -0,0 +1,229 @@
+package jav.android.console;
+
+import android.content.*;
+import android.os.*;
+import jav.android.Msg;
+
+
+interface IAndroidConsoleConstants
+{
+ ComponentName CONSOLE_APP_INTERFACE_NAME = new ComponentName("jav.android.console","jav.android.console.IAndroidConsole");
+
+ int INIT     = IBinder.FIRST_CALL_TRANSACTION + 1;
+ int DESTROY  = IBinder.FIRST_CALL_TRANSACTION + 2;
+ int WRITE    = IBinder.FIRST_CALL_TRANSACTION + 3;
+ int READ     = IBinder.FIRST_CALL_TRANSACTION + 4;
+ int ON_RECIEVE_CLIENT_ID = IBinder.FIRST_CALL_TRANSACTION + 5;
+ int ON_RECIEVE_INPUT     = IBinder.FIRST_CALL_TRANSACTION + 6;
+}
+
+public class IConsole implements ServiceConnection,
+                                 IAndroidConsoleConstants
+{
+    Context ctx;
+    int clientId;
+    IBinder serviceBinder, clientBinder;
+    String input;
+    final Object threadLock = new Object();
+
+
+    public IConsole(Context ctx) throws RuntimeException
+    {
+         this.ctx = ctx;
+         clientId = android.os.Process.myPid();
+
+         Intent intent = new Intent();
+         intent.setComponent(CONSOLE_APP_INTERFACE_NAME);
+
+         try{ ctx.bindService(intent,this,Context.BIND_AUTO_CREATE); }
+         catch(Exception e){ throw new RuntimeException("failed to connect to service : " + e + e.getMessage()); }
+    }
+
+    public void disconnect()
+    {
+     ctx.unbindService(this);
+    }
+
+    @Override public void finalize()
+    {
+     disconnect();
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name,IBinder serviceBinder)
+    {
+     this.serviceBinder = serviceBinder;
+     if(!createConsole()) disconnect();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name){}
+
+    public String readString()
+    {
+     return readConsole();
+    }
+
+    public Boolean readBool()
+    {
+     return new Boolean(input);
+    }
+
+    public Byte readByte()
+    {
+     return new Byte(input);
+    }
+
+    public Character readChar()
+    {
+     return new Character(input.charAt(0));
+    }
+
+    public Short readShort()
+    {
+     return new Short(input);
+    }
+
+    public Integer readInt()
+    {
+     return new Integer(input);
+    }
+
+    public Long readLong()
+    {
+     return new Long(input);
+    }
+
+    public Float readFloat()
+    {
+     return new Float(input);
+    }
+
+    public Double readDouble()
+    {
+     return new Double(input);
+    }
+
+    public void writeString(String data)
+    {
+     if(data == null)  writeConsole("null");
+     else writeConsole(data);
+    }
+
+    public void writeBool(boolean data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeByte(byte data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeChar(char data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeShort(short data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeInt(int data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeLong(long data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeFloat(float data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+
+    public void writeDouble(double data)
+    {
+     writeConsole(String.valueOf(data));
+    }
+/*
+    private void readInstruction(jav.rFile file)
+    {
+        try
+        {
+            switch(file.readInt())
+            {
+             case DESTROY: destroyClient(); break;
+             case ON_RECIEVE_INPUT: onRecieveInput(file.readString());
+             case ON_RECIEVE_CLIENT_ID: onRecieveClientId(file.readInt());
+            }
+        }
+        catch(Exception e){ Msg.shortToast("IConsole.readInstruction - " + e.getMessage()); }
+    }
+*/
+    private boolean createConsole()
+    {
+     Parcel data = Parcel.obtain();
+     Parcel reply = Parcel.obtain();
+
+     data.writeInt(clientId);
+     try { serviceBinder.transact(INIT,data,reply,0); }
+     catch(Exception e){ return false; }
+
+     data.recycle();
+     reply.recycle();
+     return true;
+    }
+
+    private String readConsole() throws RuntimeException
+    {/*
+        synchronized(threadLock)
+        {
+             try
+             {
+              buffer.putInt(READ);
+              buffer.putInt(clientId);
+
+                // serviceWriter.writeInt(DESTROY);
+                 //serviceWriter.writeInt(clientId);
+
+              serviceWriter.write(buffer.array(),0,buffer.position());
+              buffer.clear();
+
+              threadLock.wait();
+             }
+             catch(InterruptedException e){}
+             catch(java.io.IOException e){ Msg.box(ctx,"AnroidConsoleInterface.readConsole failed: " + e + " " + e.getMessage()); }
+        }
+
+        return input;*/
+        return null;
+    }
+
+    private boolean writeConsole(String s_data)
+    {
+         Parcel data = Parcel.obtain();
+         Parcel reply = Parcel.obtain();
+
+         try
+         {
+          data.writeInt(clientId);
+          data.writeString(s_data);
+          serviceBinder.transact(WRITE,data,reply,0);
+         }
+         catch(Exception e){ Msg.box(ctx,"failed to write to service : " + e + e.getMessage());  }
+
+         data.recycle();
+         reply.recycle();
+         return true;
+    }
+/*
+    private void onRecieveInput(String input)
+    {
+     this.input = input;
+     threadLock.notify();
+    }*/
+}
